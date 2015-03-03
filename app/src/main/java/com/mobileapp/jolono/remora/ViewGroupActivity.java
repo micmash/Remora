0de795ab08d69c00434018bcf21efd3c63af666e
Noah Torrance <torrance.8@buckeyemail.osu.edu>
2015-03-03 13:56:37 -0500

    Linked buttons on activities via button. added life cycle methods with logging.

--- app/src/main/java/com/mobileapp/jolono/remora/ViewGroupActivity.java
+++ app/src/main/java/com/mobileapp/jolono/remora/ViewGroupActivity.java
@@ -3,19 +3,59 @@ package com.mobileapp.jolono.remora;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class ViewGroupActivity extends ActionBarActivity implements View.OnClickListener{
+public class ViewGroupActivity extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_group);
+
+        findViewById(R.id.groupPageMember1Button).setOnClickListener(this);
     }
 
+    /* life cycle logging --------------------------------------------------------------------*/
+    @Override
+    protected void onStart() {
+        Log.d("life cycle", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("life cycle", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("life cycle", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("life cycle", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("life cycle", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected  void onDestroy() {
+        Log.d("life cycle", "onDestroy");
+        super.onDestroy();
+    }
+    /*---------------------------------------------------------------------------------------*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,8 +81,9 @@ public class ViewGroupActivity extends ActionBarActivity implements View.OnClick
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.button) {
-            startActivity(new Intent(this, ViewProfileActivity.class));
+        switch(v.getId()) {
+            case R.id.groupPageMember1Button:
+                this.startActivity(new Intent(this, ViewGroupActivity.class));
         }
     }
 }
--- app/src/main/java/com/mobileapp/jolono/remora/ViewProfileActivity.java
+++ app/src/main/java/com/mobileapp/jolono/remora/ViewProfileActivity.java
@@ -3,18 +3,60 @@ package com.mobileapp.jolono.remora;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
-public class ViewProfileActivity extends ActionBarActivity  implements View.OnClickListener {
-
+public class ViewProfileActivity extends ActionBarActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("life cycle", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+
+        findViewById(R.id.profilePageGroupButton).setOnClickListener(this);
     }
+
+    /* life cycle logging --------------------------------------------------------------------*/
+
+    @Override
+    protected void onStart() {
+        Log.d("life cycle", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("life cycle", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("life cycle", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("life cycle", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("life cycle", "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected  void onDestroy() {
+        Log.d("life cycle", "onDestroy");
+        super.onDestroy();
+    }
+    /*---------------------------------------------------------------------------------------*/
 
 
     @Override
@@ -41,8 +83,9 @@ public class ViewProfileActivity extends ActionBarActivity  implements View.OnCl
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.button2) {
-            startActivity(new Intent(this, ViewGroupActivity.class));
+        switch(v.getId()) {
+            case R.id.profilePageGroupButton:
+                startActivity(new Intent(this, ViewGroupActivity.class));
         }
     }
 
